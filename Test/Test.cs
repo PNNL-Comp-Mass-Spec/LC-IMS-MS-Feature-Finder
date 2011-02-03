9e949227b9fa07a3a2904b3dd7f1cf83098a24a1
Kevin Crowell <Kevin.Crowell@pnnl.gov>
2011-02-03 20:04:24 +0000

    - Added Test project and simple unit test

---
+++ Test/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using FeatureFinder;
+using System.IO;
+using System.Text.RegularExpressions;
+using FeatureFinder.Control;
+using FeatureFinder.Data.Maps;
+
+namespace Test
+{
+	public class Test
+	{
+		[Test]
+		public void PrintExampleSettings()
+		{
+			Settings.PrintExampleSettings();
+		}
+	}
+}
