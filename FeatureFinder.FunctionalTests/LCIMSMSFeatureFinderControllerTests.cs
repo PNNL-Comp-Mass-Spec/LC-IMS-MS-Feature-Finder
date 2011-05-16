947ea37b9607c2ff67b81454a1a72e01465874a3
Kevin Crowell <Kevin.Crowell@pnnl.gov>
2011-05-16 22:20:49 +0000

    adjusted test file path

--- FeatureFinder.FunctionalTests/LCIMSMSFeatureFinderControllerTests.cs
+++ FeatureFinder.FunctionalTests/LCIMSMSFeatureFinderControllerTests.cs
@@ -14,7 +14,7 @@ namespace Test
         public void standardFile_no_conformationDetection_test1()
         {
 
-            string testfile = @"C:\Users\d3x720\Documents\PNNL\My_DataAnalysis\Standard_Testing\LCMSFeatureFinder\UIMF\Parameter_Files\FF_IMS_UseHardCodedFilters_NoFlags_20ppm_Min3Pts_4MaxLCGap_NoDaCorr_NoConfDtn_2011-03-21.ini";
+            string testfile = @"\\protoapps\UserData\Slysz\Standard_Testing\LCMSFeatureFinder\UIMF\Parameter_Files\FF_IMS_UseHardCodedFilters_NoFlags_20ppm_Min3Pts_4MaxLCGap_NoDaCorr_NoConfDtn_2011-03-21.ini";
 
             IniReader iniReader = new IniReader(testfile);
             iniReader.CreateSettings();
