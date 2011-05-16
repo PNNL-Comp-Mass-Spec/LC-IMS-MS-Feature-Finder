d6eae651b960f27b6097f963550b79390e926d9b
Kevin Crowell <Kevin.Crowell@pnnl.gov>
2011-05-16 22:16:13 +0000

    adjusted functional test so it works off protoapps

--- FeatureFinder.FunctionalTests/LCIMSMSFeatureFinderControllerTests.cs
+++ FeatureFinder.FunctionalTests/LCIMSMSFeatureFinderControllerTests.cs
@@ -11,10 +11,10 @@ namespace Test
     public class LCIMSMSFeatureFinderControllerTests
     {
         [Test]
-        public void Test1()
+        public void standardFile_no_conformationDetection_test1()
         {
 
-            string testfile = @"C:\Users\d3x720\Documents\PNNL\My_DataAnalysis\Standard_Testing\LCMSFeatureFinder\UIMF\Parameter_Files\FF_IMS_UseHardCodedFilters_NoFlags_20ppm_Min3Pts_4MaxLCGap_NoDaCorr_ConfDtn_2011-03-21.ini";
+            string testfile = @"C:\Users\d3x720\Documents\PNNL\My_DataAnalysis\Standard_Testing\LCMSFeatureFinder\UIMF\Parameter_Files\FF_IMS_UseHardCodedFilters_NoFlags_20ppm_Min3Pts_4MaxLCGap_NoDaCorr_NoConfDtn_2011-03-21.ini";
 
             IniReader iniReader = new IniReader(testfile);
             iniReader.CreateSettings();
@@ -24,6 +24,22 @@ namespace Test
             LCIMSMSFeatureFinderController controller = new LCIMSMSFeatureFinderController(isosReader);
             controller.Execute();
 	
+        }
+
+        [Test]
+        public void standardFile_conformationDetection_test1()
+        {
+
+            string testfile = @"\\protoapps\UserData\Slysz\Standard_Testing\LCMSFeatureFinder\UIMF\Parameter_Files\FF_IMS_UseHardCodedFilters_NoFlags_20ppm_Min3Pts_4MaxLCGap_NoDaCorr_ConfDtn_2011-03-21.ini";
+
+            IniReader iniReader = new IniReader(testfile);
+            iniReader.CreateSettings();
+
+            IsosReader isosReader = new IsosReader();
+
+            LCIMSMSFeatureFinderController controller = new LCIMSMSFeatureFinderController(isosReader);
+            controller.Execute();
+
         }
 
     }
