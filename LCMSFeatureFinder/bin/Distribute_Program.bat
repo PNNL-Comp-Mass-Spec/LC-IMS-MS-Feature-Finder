b42274b7f55ecdce8224f6bc9559502ee4ccbf2b
alchemistmatt <matthew.monroe@pnnl.gov>
2025-10-22 18:40:55 -0700

    Update batch distribution file

--- LCMSFeatureFinder/bin/Distribute_Program.bat
+++ LCMSFeatureFinder/bin/Distribute_Program.bat
@@ -4,10 +4,10 @@ xcopy Debug\FeatureFinder.pdb       C:\DMS_Programs\LCMSFeatureFinder /D /Y
 xcopy Debug\LCMSFeatureFinder.pdb   C:\DMS_Programs\LCMSFeatureFinder /D /Y
 xcopy ..\..\Readme.md                  C:\DMS_Programs\LCMSFeatureFinder /D /Y
 
-xcopy Debug\*.exe                   \\pnl\projects\omicssw\dms_programs\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
-xcopy Debug\*.dll                   \\pnl\projects\omicssw\dms_programs\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y /S
-xcopy Debug\FeatureFinder.pdb       \\pnl\projects\omicssw\dms_programs\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
-xcopy Debug\LCMSFeatureFinder.pdb   \\pnl\projects\omicssw\dms_programs\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
-xcopy ..\..\Readme.md                  \\pnl\projects\omicssw\dms_programs\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
+xcopy Debug\*.exe                   \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
+xcopy Debug\*.dll                   \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y /S
+xcopy Debug\FeatureFinder.pdb       \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
+xcopy Debug\LCMSFeatureFinder.pdb   \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
+xcopy ..\..\Readme.md                  \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\LCMSFeatureFinder /D /Y
 
 pause
